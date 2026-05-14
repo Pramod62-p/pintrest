533f9fa31df0214bf584a8a1c36ddfc4c6aafa05
Pramod62-p <pramodsagar6162@gmail.com>
2026-05-14 11:45:16 +0530

    Update page.js

--- pintrest/src/app/page.js
+++ pintrest/src/app/page.js
@@ -1,7 +1,11 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import {useRouter} from "next/navigation";
 
 export default function Home() {
+  const rout = useRouter();
   return (
     <div className={styles.page} onClick={() => rout.push("./login")}>Click Here to Login</div>
   );
