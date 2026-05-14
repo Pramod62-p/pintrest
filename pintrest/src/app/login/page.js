7c317957e6cc22483e4d2eee1e9a2080312c720c
Pramod62-p <pramodsagar6162@gmail.com>
2026-05-14 18:16:53 +0530

    Update page.js

--- pintrest/src/app/login/page.js
+++ pintrest/src/app/login/page.js
@@ -7,23 +7,59 @@ import styles from "./page.module.css";
 export default function Login() {
   return (
     <div className={styles.container}>
-      <form className={styles.box}>
-        <h2 className={styles.title}>Admin Login</h2>
+  <div className={styles.card}>
+    
+    <div className={styles.logo}>A</div>
 
+    <h1 className={styles.title}>Welcome Back</h1>
+
+    <p className={styles.subtitle}>
+      Login to continue your account
+    </p>
+
+    <form className={styles.form}>
+
+      <div className={styles.inputGroup}>
         <input
           type="text"
           placeholder="Username"
           className={styles.input}
         />
+      </div>
 
+      <div className={styles.inputGroup}>
         <input
           type="password"
           placeholder="Password"
           className={styles.input}
         />
 
-        <button type="submit" className={styles.button}>Login</button>
-      </form>
-    </div>
+        <span className={styles.show}>
+          Show
+        </span>
+      </div>
+
+      <div className={styles.row}>
+        <label className={styles.remember}>
+          <input type="checkbox" />
+          Remember me
+        </label>
+
+        <a href="#" className={styles.link}>
+          Forgot Password?
+        </a>
+      </div>
+
+      <button type="submit" className={styles.button}>
+        Login
+      </button>
+
+      <p className={styles.footer}>
+        Don&apos;t have an account? <span>Sign Up</span>
+      </p>
+
+    </form>
+  </div>
+</div>
   );
 }
